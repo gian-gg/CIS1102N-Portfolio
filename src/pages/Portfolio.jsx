784f3aaf0b20cb836444682f363efbfc8c9fec6e
spookyexe <gianepanto@gmail.com>
2024-12-05 02:00:47 +0800

    FEAT: Scroll on Top on Next Page, Render h2s

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Link, NavLink } from "react-router-dom";
+import { Link as LinkScroll, animateScroll as scroll } from "react-scroll";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import remarkCustomHeaderId from "remark-custom-header-id";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
@@ -20,6 +22,9 @@ const NavButton = ({ direction, number }) => {
   return (
     <Link
       to={disabled ? "#" : `/${number}`}
+      onClick={() => {
+        scroll.scrollToTop();
+      }}
       className={`h-14 w-36 md:h-16 md:w-40 bg-[#DFDFDF] dark:bg-[#19171A] rounded-2xl border-4 border-[#CACACA] dark:border-[#303030] flex justify-between items-center px-4 hover:opacity-60 ${
         disabled ? "cursor-not-allowed opacity-60" : ""
       }`}
@@ -62,6 +67,10 @@ const Portfolio = () => {
   }, [number]);
 
   const [filesContent, setFilesContent] = useState([]);
+  const [onThisPage, setOnThisPage] = useState({
+    headings: [], // Initialize with an empty array
+    content: "", // Initialize with an empty string
+  });
 
   const markdownFiles = [
     "/portfolios/1.md",
@@ -73,7 +82,7 @@ const Portfolio = () => {
   ];
 
   useEffect(() => {
-    const fetchFiles = async () => {
+    const fetchH1 = async () => {
       const fileContents = await Promise.all(
         markdownFiles.map(async (file) => {
           const response = await fetch(file);
@@ -92,8 +101,25 @@ const Portfolio = () => {
       setFilesContent(fileContents);
     };
 
-    fetchFiles();
+    fetchH1();
   }, []);
+
+  useEffect(() => {
+    const fetchH2 = async () => {
+      const response = await fetch(`/portfolios/${number}.md`);
+      const data = await response.text();
+
+      const headings = data.match(/^##\s(.*)$/gm);
+      const allHeadings = headings || ["No Heading Found"];
+
+      setOnThisPage({
+        headings: allHeadings,
+        content: data,
+      });
+    };
+
+    fetchH2();
+  }, [number]);
 
   return (
     <div className="w-full h-full bg-light dark:bg-dark bg-[url('../assets/images/bg/bgLight.png')] dark:bg-[url('../assets/images/bg/bgDark.png')] bg-repeat-y bg-center ">
@@ -114,7 +140,7 @@ const Portfolio = () => {
                       filesContent.map((file, index) => (
                         <li key={index}>
                           <NavLink
-                            to={`/${index + 1}`}
+                            to={`${index + 1}`}
                             className={({ isActive }) =>
                               `opacity-80 hover:dark:text-purpleD hover:text-purpleL font-medium hover:opacity-100 ${
                                 isActive
@@ -136,7 +162,7 @@ const Portfolio = () => {
                   <div className="markdown-container">
                     <ReactMarkdown
                       children={content}
-                      remarkPlugins={[remarkGfm]}
+                      remarkPlugins={[remarkGfm, remarkCustomHeaderId]}
                     />
                   </div>
                   <hr className="border-light w-4/5 mt-20" />
@@ -149,6 +175,26 @@ const Portfolio = () => {
                   <h1 className="font-poppins font-semibold text-lg text-dark dark:text-light">
                     On This Page:
                   </h1>
+                  <ul className="font-poppins flex flex-col gap-4">
+                    {onThisPage.headings && onThisPage.headings.length > 0 ? (
+                      onThisPage.headings.map((heading, index) => {
+                        const id = heading.toLowerCase().replace(/\s+/g, "-");
+                        return (
+                          <li key={index}>
+                            <LinkScroll
+                              to={id}
+                              smooth={true}
+                              className="text-dark dark:text-light opacity-80 hover:dark:text-purpleD hover:text-purpleL font-medium hover:opacity-100"
+                            >
+                              <ReactMarkdown>{heading}</ReactMarkdown>
+                            </LinkScroll>
+                          </li>
+                        );
+                      })
+                    ) : (
+                      <Loading />
+                    )}
+                  </ul>
                 </div>
               </div>
             ) : (
